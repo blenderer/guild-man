5b5c16586675caab118cda822ac5f5f6213d2df4
Eric Harrison <eric.h@rrison.io>
2020-10-30 00:54:06 -0400

    fix lint

--- src/Components/Quest/StartQuestDialog/index.js
+++ src/Components/Quest/StartQuestDialog/index.js
@@ -10,7 +10,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import _get from 'lodash/get';
+// import _get from 'lodash/get';
 
 const styles = theme => ({
 
@@ -40,7 +40,10 @@ class StartQuestDialog extends Component {
   };
 
   render() {
-    const { user, quest } = this.props;
+    const {
+      // user,
+      quest
+    } = this.props;
 
     // const availableQuests = _get(user, 'quests.available');
 
